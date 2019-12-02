12de6fc7b07d39dc5eb2a1c449259346dfefd36d
Kenji Chao <cchao1@alumni.cmu.edu>
2019-12-01 21:46:13 -0800

    Day 1 part 2

--- day1/main.go
+++ day1/main.go
@@ -17,22 +17,34 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	totalFuel := 0
+	totalFuelPart1 := 0
+	totalFuelPart2 := 0
 	for scanner.Scan() {
 		i, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			log.Fatal(err)
 		}
-		totalFuel += RequiredFuel(i)
+		totalFuelPart1 += RequiredFuel(i)
+		totalFuelPart2 += RequiredActualFuel(i)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Total Fuel: ", totalFuel)
+	fmt.Println("Total Fuel Part1: ", totalFuelPart1)
+	fmt.Println("Total Fuel Part2: ", totalFuelPart2)
 }
 
 func RequiredFuel(mass int) int {
 	return int(math.Floor(float64(mass/3)) - 2)
+}
+
+func RequiredActualFuel(mass int) (total int) {
+	fuel := RequiredFuel(mass)
+	for fuel > 0 {
+		total += fuel
+		fuel = RequiredFuel(fuel)
+	}
+	return
 }
--- day1/main_test.go
+++ day1/main_test.go
@@ -23,3 +23,21 @@ func TestRequiredFuel(t *testing.T) {
 		})
 	}
 }
+
+func TestRequiredActualFuel(t *testing.T) {
+	tests := []struct {
+		mass int
+		want int
+	}{
+		{14, 2},
+		{1969, 966},
+		{100756, 50346},
+	}
+	for _, tt := range tests {
+		t.Run("Test for mass "+strconv.Itoa(tt.mass), func(t *testing.T) {
+			if got := RequiredActualFuel(tt.mass); got != tt.want {
+				t.Errorf("RequiredFuel() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

b4c948f9efbccfa10b1473448ada20e801dc6d56
Kenji Chao <cchao1@alumni.cmu.edu>
2019-12-06 23:09:35 -0800

    Day 7 part2

--- day7/main.go
+++ day7/main.go
@@ -18,13 +18,30 @@ const OpcodeLessThan = 7
 const OpcodeEquals = 8
 const OpcodeHalt = 99
 
+type Amplifier struct {
+	input           []int
+	usePhaseSetting bool
+	index           int
+}
+
+type AmplifierSystem struct {
+	phaseSettings int
+	ampA          Amplifier
+	ampB          Amplifier
+	ampC          Amplifier
+	ampD          Amplifier
+	ampE          Amplifier
+}
+
 func main() {
 	lines := util.ReadLines("day7/input.txt")
 
 	input := toIntArray(strings.Split(lines[0], ","))
 
-	maxOutput, phaseSettings := Amplifier(input)
+	maxOutput, phaseSettings := MaxOutput(input, false)
 	fmt.Println("Max output signal: ", maxOutput, phaseSettings)
+	maxOutput, phaseSettings = MaxOutput(input, true)
+	fmt.Println("Feedback Max output signal: ", maxOutput, phaseSettings)
 }
 
 func toIntArray(input []string) []int {
@@ -39,7 +56,7 @@ func toIntArray(input []string) []int {
 	return output
 }
 
-func Amplifier(input []int) (int, int) {
+func MaxOutput(input []int, feedbackLoop bool) (int, int) {
 	maxOutput := 0
 	phaseSettings := 0
 
@@ -50,6 +67,17 @@ func Amplifier(input []int) (int, int) {
 		3: true,
 		4: true,
 	}
+
+	if feedbackLoop {
+		openSet = map[int]bool{
+			5: true,
+			6: true,
+			7: true,
+			8: true,
+			9: true,
+		}
+	}
+
 	for i, oki := range openSet {
 		if oki {
 			openSet[i] = false
@@ -65,7 +93,8 @@ func Amplifier(input []int) (int, int) {
 									for m, okm := range openSet {
 										if okm {
 											openSet[m] = false
-											output := runAmplifier(input, i, j, k, l, m)
+
+											output := initAndRun(input, i, j, k, l, m, feedbackLoop)
 											if output > maxOutput {
 												maxOutput = output
 												phaseSettings = i*10000 + j*1000 + k*100 + l*10 + m
@@ -89,20 +118,69 @@ func Amplifier(input []int) (int, int) {
 	return maxOutput, phaseSettings
 }
 
-func runAmplifier(input []int, phase1, phase2, phase3, phase4, phase5 int) int {
-	output1 := Intcode(input, phase1, 0)
-	output2 := Intcode(input, phase2, output1)
-	output3 := Intcode(input, phase3, output2)
-	output4 := Intcode(input, phase4, output3)
-	output5 := Intcode(input, phase5, output4)
+func initAndRun(input []int, i, j, k, l, m int, feedbackLoop bool) int {
+	a := &AmplifierSystem{
+		phaseSettings: i*10000 + j*1000 + k*100 + l*10 + m,
+		ampA:          Amplifier{append([]int(nil), input...), true, 0},
+		ampB:          Amplifier{append([]int(nil), input...), true, 0},
+		ampC:          Amplifier{append([]int(nil), input...), true, 0},
+		ampD:          Amplifier{append([]int(nil), input...), true, 0},
+		ampE:          Amplifier{append([]int(nil), input...), true, 0},
+	}
+
+	output := 0
+	if feedbackLoop {
+		output = a.runFeedbackLoop()
+	} else {
+		output = a.run()
+	}
+	return output
+}
+
+func (a *AmplifierSystem) run() int {
+	output1 := a.ampA.Intcode(a.phaseSettings/10000, 0)
+	output2 := a.ampB.Intcode(a.phaseSettings/1000%10, output1)
+	output3 := a.ampC.Intcode(a.phaseSettings/100%10, output2)
+	output4 := a.ampD.Intcode(a.phaseSettings/10%10, output3)
+	output5 := a.ampE.Intcode(a.phaseSettings%10, output4)
 	return output5
 }
 
-func Intcode(original []int, phaseCode int, inputValue int) int {
-	input := append([]int(nil), original...)
-	index := 0
-	for index < len(input) {
-		opcode, parameterMode := input[index]%100, input[index]/100
+func (a *AmplifierSystem) runFeedbackLoop() int {
+	output := 0
+	input := 0
+	for true {
+		output1 := a.ampA.Intcode(a.phaseSettings/10000, input)
+		if output1 == -1 {
+			break
+		}
+		output2 := a.ampB.Intcode(a.phaseSettings/1000%10, output1)
+		if output2 == -1 {
+			break
+		}
+		output3 := a.ampC.Intcode(a.phaseSettings/100%10, output2)
+		if output3 == -1 {
+			break
+		}
+		output4 := a.ampD.Intcode(a.phaseSettings/10%10, output3)
+		if output4 == -1 {
+			break
+		}
+		output5 := a.ampE.Intcode(a.phaseSettings%10, output4)
+		if output5 == -1 {
+			break
+		}
+		input = output5
+		output = output5
+	}
+
+	return output
+}
+
+func (a *Amplifier) Intcode(phaseCode int, inputValue int) int {
+	index := a.index
+	for index < len(a.input) {
+		opcode, parameterMode := a.input[index]%100, a.input[index]/100
 		if opcode == OpcodeHalt {
 			break
 		}
@@ -112,31 +190,31 @@ func Intcode(original []int, phaseCode int, inputValue int) int {
 			if parameterModes[2] == Immediate {
 				log.Fatal("Can't have immediate mode on write operation - Add")
 			}
-			input[input[index+3]] = parameterValue(input, index+1, parameterModes[0]) + parameterValue(input, index+2, parameterModes[1])
+			a.input[a.input[index+3]] = parameterValue(a.input, index+1, parameterModes[0]) + parameterValue(a.input, index+2, parameterModes[1])
 			index += 4
 		case OpcodeMultiple:
 			parameterModes := parameterModeList(parameterMode, 3)
 			if parameterModes[2] == Immediate {
 				log.Fatal("Can't have immediate mode on write operation - Multiple")
 			}
-			input[input[index+3]] = parameterValue(input, index+1, parameterModes[0]) * parameterValue(input, index+2, parameterModes[1])
+			a.input[a.input[index+3]] = parameterValue(a.input, index+1, parameterModes[0]) * parameterValue(a.input, index+2, parameterModes[1])
 			index += 4
 		case OpcodeInput:
 			parameterModes := parameterModeList(parameterMode, 1)
 			if parameterModes[0] == Immediate {
 				log.Fatal("Can't have immediate mode on write operation - Input")
 			}
-			if index == 0 {
-				input[input[index+1]] = phaseCode
-				//fmt.Println("OpcodeInput, phaseCode: ", phaseCode)
+			if a.usePhaseSetting {
+				a.input[a.input[index+1]] = phaseCode
+				a.usePhaseSetting = false
 			} else {
-				input[input[index+1]] = inputValue
-				//fmt.Println("OpcodeInput, inputValue: ", inputValue)
+				a.input[a.input[index+1]] = inputValue
 			}
 			index += 2
 		case OpcodeOutput:
 			parameterModes := parameterModeList(parameterMode, 1)
-			output := parameterValue(input, index+1, parameterModes[0])
+			output := parameterValue(a.input, index+1, parameterModes[0])
+			a.index = index + 2
 			return output
 			//if output == 0 {
 			//	fmt.Println("Test passed! index:", index)
@@ -148,8 +226,8 @@ func Intcode(original []int, phaseCode int, inputValue int) int {
 			//index += 2
 		case OpcodeJumpIfTrue:
 			parameterModes := parameterModeList(parameterMode, 2)
-			firstParameter := parameterValue(input, index+1, parameterModes[0])
-			secondParameter := parameterValue(input, index+2, parameterModes[1])
+			firstParameter := parameterValue(a.input, index+1, parameterModes[0])
+			secondParameter := parameterValue(a.input, index+2, parameterModes[1])
 			if firstParameter != 0 {
 				index = secondParameter
 			} else {
@@ -157,8 +235,8 @@ func Intcode(original []int, phaseCode int, inputValue int) int {
 			}
 		case OpcodeJumpIfFalse:
 			parameterModes := parameterModeList(parameterMode, 2)
-			firstParameter := parameterValue(input, index+1, parameterModes[0])
-			secondParameter := parameterValue(input, index+2, parameterModes[1])
+			firstParameter := parameterValue(a.input, index+1, parameterModes[0])
+			secondParameter := parameterValue(a.input, index+2, parameterModes[1])
 			if firstParameter == 0 {
 				index = secondParameter
 			} else {
@@ -166,24 +244,24 @@ func Intcode(original []int, phaseCode int, inputValue int) int {
 			}
 		case OpcodeLessThan:
 			parameterModes := parameterModeList(parameterMode, 3)
-			firstParameter := parameterValue(input, index+1, parameterModes[0])
-			secondParameter := parameterValue(input, index+2, parameterModes[1])
-			thirdParameter := input[index+3]
+			firstParameter := parameterValue(a.input, index+1, parameterModes[0])
+			secondParameter := parameterValue(a.input, index+2, parameterModes[1])
+			thirdParameter := a.input[index+3]
 			if firstParameter < secondParameter {
-				input[thirdParameter] = 1
+				a.input[thirdParameter] = 1
 			} else {
-				input[thirdParameter] = 0
+				a.input[thirdParameter] = 0
 			}
 			index += 4
 		case OpcodeEquals:
 			parameterModes := parameterModeList(parameterMode, 3)
-			firstParameter := parameterValue(input, index+1, parameterModes[0])
-			secondParameter := parameterValue(input, index+2, parameterModes[1])
-			thirdParameter := input[index+3]
+			firstParameter := parameterValue(a.input, index+1, parameterModes[0])
+			secondParameter := parameterValue(a.input, index+2, parameterModes[1])
+			thirdParameter := a.input[index+3]
 			if firstParameter == secondParameter {
-				input[thirdParameter] = 1
+				a.input[thirdParameter] = 1
 			} else {
-				input[thirdParameter] = 0
+				a.input[thirdParameter] = 0
 			}
 			index += 4
 		default:
